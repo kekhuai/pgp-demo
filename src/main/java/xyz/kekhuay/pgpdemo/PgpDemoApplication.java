389b102c22770fa14714b04d331eaa5405abd7c6
Natthaphon Phanit <natthaphon.pha@ascendcorp.com>
2019-01-17 20:09:50 +0700

    first commit

---
+++ src/main/java/xyz/kekhuay/pgpdemo/PgpDemoApplication.java
@@ -0,0 +1,14 @@
+package xyz.kekhuay.pgpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpDemoApplication.class, args);
+	}
+
+}
+
